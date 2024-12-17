0936445547a1fe13493e1515d3cb61cba1adfe20
Salvatore D'Amico <salvatore.damico@hotmail.it>
2024-12-17 18:51:53 +0100

    cosi escono le funzioni senza parametri e le classi senza prop

--- UEDumper/Engine/UEClasses/UnrealClasses.h
+++ UEDumper/Engine/UEClasses/UnrealClasses.h
@@ -193,20 +193,25 @@ class UStruct : public UField
 public:
 	using UField::UField;
 
-	unsigned char                                      UnknownData00_3[0x18];
-	UStruct* SuperStruct;
-	unsigned char                                      UnknownData01_6[0x8];
-	UField* Children;
-	unsigned char                                      UnknownData02_7[0x58];
-	
-	int32_t		PropertiesSize;
-	int32_t		MinAlignment;
-	TArray<uint8_t> Script;
-	UProperty* PropertyLink;
-	UProperty* RefLink;
-	UProperty* DestructorLink;
-	UProperty* PostConstructLink;
-	TArray<UObject*> ScriptObjectReferences;
+	//start offset 0x38 Size 0xDC
+
+	unsigned char UnknownData00_3[0x10];	 // 0x0038 (0x0010)
+	int32_t		PropertiesSize;				 // 0x0048 (0x0004)
+	unsigned char UnknownData00_4[0x4];		 // 0x004C (0x0004)
+	UStruct* SuperStruct;					 // 0x0050 (0x0008)
+	int32_t		MinAlignment;				 // 0x0058 (0x0004)
+	unsigned char UnknownData00_5[0x4];      // 0x005C (0x0004)
+	UField* Children;						 // 0x0080 (0x0008)
+	unsigned char UnknownData00_6[0x20];     // 0x0060 (0x0020)
+	unsigned char UnknownData02_7[0x8C];     // 0x0088 (0x008C) 
+
+
+	//TArray<uint8_t> Script;
+	//UProperty* PropertyLink;
+	//UProperty* RefLink;
+	//UProperty* DestructorLink;
+	//UProperty* PostConstructLink;
+	//TArray<UObject*> ScriptObjectReferences;
 
 
 	template <typename T>
